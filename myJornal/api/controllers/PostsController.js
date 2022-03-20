11d8cfcc56004c6ae07552512f4f780982c17781
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-03-20 20:22:33 -0300

    create second post

--- myJornal/api/controllers/PostsController.js
+++ myJornal/api/controllers/PostsController.js
@@ -1,6 +1,17 @@
 module.exports = {
     posts: function(req, res) {
-        const postOne = {id: 1, title: "POST TITLE 1", body: "HERE IS MY BODY"}
-        res.send(postOne)
+        const postOne = {
+            id: 1, 
+            title: "POST TITLE 1", 
+            body: "HERE IS MY BODY"
+        }
+
+        const postTwo = {
+            id: 2, 
+            title: "POST TITLE 2", 
+            body: "HERE IS MY BODY 2"
+        }
+
+        res.send([postOne, postTwo])
     }
 }
